005b76d789d1e6cd880b960391c801beea6360fe
Düpmeier, Jan <jan.duepmeier@dm.de>
2019-04-25 13:39:25 +0200

    add error for other pass errors (like no secret key)

--- pass.go
+++ pass.go
@@ -14,7 +14,7 @@ var (
 	ErrPassExecNotFound error = errors.New("could not find pass executable")
 )
 
-// KeyNotInStoreError is a Error that is returned if the key is not in
+// KeyNotInStoreError is an Error that is returned if the key is not in
 // the password store.
 type KeyNotInStoreError struct {
 	key string
@@ -25,10 +25,29 @@ func (err KeyNotInStoreError) Error() string {
 	return fmt.Sprintf("%s is not in the password store", err.key)
 }
 
+// PassExitError is an Error that is returned if the pass command has exited
+// with an error.
+type PassExitError struct {
+	Message string
+	Err     error
+}
+
+// Error retuns the message that pass has exited with an error.
+func (err PassExitError) Error() string {
+	return fmt.Sprintf("pass has exited with the following message: %s", err.Message)
+}
+
 // newKeyNotInStoreError is a helper method to create a KeyNotInStoreError
 func newKeyNotInStoreError(key string) error {
 	return KeyNotInStoreError{
 		key: key,
+	}
+}
+
+func newPassExitError(err *exec.ExitError) error {
+	return PassExitError{
+		Message: string(err.Stderr),
+		Err:     err,
 	}
 }
 
@@ -39,19 +58,32 @@ func newKeyNotInStoreError(key string) error {
 // on the host. If the command is not installed the method
 // will return the error ErrPassExecNotFound.
 //
-// If the given key is not in the password store a
+// If the given key is not in the password store an
 // KeyNotInStoreError is returned.
+//
+// If the pass command has exited with an other error an
+// PassExitError is returned.
 func GetFromPass(key string) (string, error) {
 	var pass string
 
+	// check if the pass executable is in path.
 	path, err := exec.LookPath("pass")
 	if err != nil {
 		return pass, ErrPassExecNotFound
 	}
 
+	// the command to execute is 'pass show <key>'
 	out, err := exec.Command(path, "show", key).Output()
 	if err != nil {
-		return pass, newKeyNotInStoreError(key)
+		// if the command fails the key is not in the password store.
+		if exitError, ok := err.(*exec.ExitError); ok {
+			switch exitError.ExitCode() {
+			case 1:
+				return pass, newKeyNotInStoreError(key)
+			default:
+				return pass, newPassExitError(exitError)
+			}
+		}
 	}
 
 	splits := strings.Split(string(out), "\n")
