86895a8372de6d1315a9c50234cd5d5a1a861abc
Düpmeier, Jan <jan.duepmeier@dm.de>
2019-04-25 13:23:13 +0200

    add documentation

--- pass.go
+++ pass.go
@@ -1,3 +1,5 @@
+// Package getfrompass provides a helper method to get passwords
+// from the passwordmanager pass (https://www.passwordstore.org).
 package getfrompass
 
 import (
@@ -12,20 +14,33 @@ var (
 	ErrPassExecNotFound error = errors.New("could not find pass executable")
 )
 
+// KeyNotInStoreError is a Error that is returned if the key is not in
+// the password store.
 type KeyNotInStoreError struct {
 	key string
 }
 
+// Error returns the message which key is not in the password store.
 func (err KeyNotInStoreError) Error() string {
 	return fmt.Sprintf("%s is not in the password store", err.key)
 }
 
+// newKeyNotInStoreError is a helper method to create a KeyNotInStoreError
 func newKeyNotInStoreError(key string) error {
 	return KeyNotInStoreError{
 		key: key,
 	}
 }
 
+// GetFromPass returns the password from the
+// command 'pass' (see https://www.passwordstore.org/).
+//
+// To use this method the command 'pass' must be installed
+// on the host. If the command is not installed the method
+// will return the error ErrPassExecNotFound.
+//
+// If the given key is not in the password store a
+// KeyNotInStoreError is returned.
 func GetFromPass(key string) (string, error) {
 	var pass string
 
